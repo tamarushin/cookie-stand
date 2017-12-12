2421a2624246302eace9967a4eb2c09fc732c926
tamarushin <tamarushin@gmail.com>
2017-12-11 16:11:15 -0800

    initial

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+var pikeStore = {
+  storeName: '1st and Pike',
+  minCust: 23,
+  maxCust: 65,
+  avgPerCust: 6.3
+  //create object literals for all stores.
+};
+console.log(pikeStore);
